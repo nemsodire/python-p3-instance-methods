282a5e5c8be85e3ef76f9a32a158d09c3ee8dfa2
neema <neemabridgetteodire2@gmail.com>
2024-05-26 13:50:29 +0300

    instances labs

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,19 @@
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
+
+# Creating instances of Dog and calling their methods
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
+snoopy.sit()
+
--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,14 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+        
+    def walk(self):
+        print("The person is walking.")
+
+Guido = Person()
+Guido.talk()
+
+Guido = Person()
+Guido.walk()
